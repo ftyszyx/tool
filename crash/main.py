9c1fcfbb6a049f1f010e431d7ba54fc0965d94f6
zyx <whyzi@qq.com>
2020-03-30 10:11:13 +0800

    zyx

--- crash/main.py
+++ crash/main.py
@@ -1,13 +1,13 @@
 import os
 import json
+import xlwt
 
-#dict_keys(['logcat', 'Brand', 'Model', 'pid', 'network info', 'memory info', 'App version', 'pname', 'Manufacturer', 'Rooted', 'open files', 'other threads', 'OS version', 'Kernel version', 'ABI list', 'Start time', 'foreground', 'Build fingerprint', 'App ID', 'Cras
-#h type', 'API level', 'ABI', 'Crash time', 'Tombstone maker'])
+#dict_keys(['logcat', 'Brand', 'Model', 'pid', 'network info', 'memory info', 'App version', 'pname',
+# 'Manufacturer', 'Rooted', 'open files', 'other threads', 'OS version', 'Kernel version', 'ABI list', 'Start time', 'foreground', 'Build fingerprint', 'App ID', 'Crash type',
+# 'API level', 'ABI', 'Crash time', 'Tombstone maker'])
 
 def parseOneFile(filepath):
-     dict={}
-     uid=filepath.split("_")[1]
-     dict["uid"]=uid
+     uid=os.path.basename(filepath).split("_")[1].split(".")[0]
      with open(filepath,"r", encoding='utf8') as f:
             ftext=f.read()
             if ftext.startswith("{"):
@@ -21,6 +21,7 @@ def parseOneFile(filepath):
                     exit(-1)
                 else:
                     #print(filejson.keys())
+                    filejson["uid"]=uid
                     return filejson
 
             else:
@@ -28,18 +29,38 @@ def parseOneFile(filepath):
      return  None
 
 
-def solvepath(filepath):
+def solvepath(filepath,excelpath):
     g=os.walk(filepath)
-    crashlist=[]
+    workbook = xlwt.Workbook()  # 新建一个工作簿
+    sheet = workbook.add_sheet("sheet1")  # 在工
+
+    sheet.write(0,0,"uid")
+    sheet.write(0, 1, "crash_Type")
+    sheet.write(0, 2, "version")
+    sheet.write(0, 3, "device")
+    sheet.write(0, 4, "log")
+
     #处理所有文件
+    row=0
     for path,dirlist,filelist in g:
         for filename in filelist:
             subfilepath=os.path.join(path,filename)
             print(subfilepath)
             crashitem=parseOneFile(subfilepath)
             if crashitem!=None:
-                crashlist.append(crashitem)
+                row+=1
+                sheet.write(row, 0, crashitem["uid"])
+                sheet.write(row, 1, crashitem["Crash type"])
+                sheet.write(row, 2, crashitem["OS version"])
+                sheet.write(row, 3,crashitem["Build fingerprint"])
+                logtext=crashitem["logcat"]
+                if(logtext.__len__()>10000):
+                    logtext=logtext[-9999:]
+                sheet.write(row, 4, logtext)
+
+    workbook.save(excelpath)
+
     #生成excel
 
 if __name__=="__main__":
-    solvepath("C:\\Users\\zyx\\Desktop\\crash_202003272")
+    solvepath("C:\\Users\\zyx\\Desktop\\crash_202003272","C:\\Users\\zyx\\Desktop\\crashexcel.xls")
